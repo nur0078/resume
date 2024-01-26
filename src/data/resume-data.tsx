101d8a14e3594f922609497ee94f66390413d64d
Anurodh Pandey <anurodhp7@gmail.com>
2024-01-26 20:31:04 +1100

    push to github

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,160 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
+  AnuIcon,
+  CarHubLogo,
   Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
+  NikeLogo,
+  TheStarLogo,
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { GlobeIcon } from "lucide-react";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Anurodh Pandey",
+  initials: "AP",
+  location: "Sydney, Australia",
+  locationLink: "https://www.google.com/maps/place/Sydney/",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Full-stack Developer from Sydney, Australia.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
+    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I work mostly with TypeScript, React, Node.js, and GraphQL.",
+
+  avatarUrl: "https://www.anurodhpandey.com.np/assets/og-aae58940.png",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "anurodhp7@gmail.com",
+    tel: "+61450380690",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/nur0078",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/pandeyanurodh/",
         icon: LinkedInIcon,
       },
       {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
+        name: "URK",
+        url: "https://www.anurodhpandey.com.np/",
+        icon: GlobeIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Federation University of Australia",
+      degree: "Bachelor's Degree in Information Technology",
+      start: "2020",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
+      company: "Buzzketing",
+      link: "https://buzzketing.com.au/",
+      badges: ["Intern"],
+      title: "Full-stack Developer Intern",
+      logo: AnuIcon,
+      start: "2023",
+      end: "2023",
+      description:
+        "Implemented in developing key features fo a React.js application leading to a 15% increase in user engagement, and started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+    },
+    {
+      company: "The Star Entertainment Group",
+      link: "https://www.starentertainmentgroup.com.au/",
+      badges: [],
+      title: "Table Games Dealer",
+      logo: TheStarLogo,
+      start: "2022",
       end: "2024",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Engaged with a dynamic array of customers, providing exceptional service within the upscale ambiance of a 5-star casino, averaging interactions with over 200+ patrons. ",
     },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
+
   ],
   skills: [
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
+    "React/Next.js/Redux",
     "Node.js",
     "GraphQL",
-    "Relay",
-    "WebRTC",
+    "TailwindCSS",
+    "MongoDB",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Nike",
       techStack: [
         "Side Project",
-        "TypeScript",
+        "JavaScript",
         "Next.js",
         "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Tailwind",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Nike Landing Page",
+      logo: NikeLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "Project Nike",
+        href: "https://nike-three-iota.vercel.app/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Car Hub",
+      techStack: ["Side Project", "TypeScript", "Next.js", "ReactJS"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Showcase website for car dealers.",
+      logo: CarHubLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "Car Hub",
+        href: "https://car-showroom-five.vercel.app/",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "Personal Portfolio",
+      techStack: ["Side Project", "Next.js", "ReactJS", "TypeScript", "Vite"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+        "Personal Portfolio Website",
+      logo: AnuIcon,
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "anurodhpandey.com.np",
+        href: "https://anurodhpandey.com.np/",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "myTravels",
+      techStack: ["Side Project", "Next.js", "TypeScript", "Figma", "ReactJS",],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
+        "Personal travel journal.",
       logo: Howdy,
       link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
+        label: "myTravels",
+        href: "https://travel-journal-bice.vercel.app/",
       },
     },
     {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      title: "Park-It",
+      techStack: ["Side Project", "Next.js", "Javscript", "Google API"],
       description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
+        "Personal parking assitance app.",
+      logo: Howdy,
       link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
+        label: "ParkIt",
+        href: "https://park-it-five.vercel.app/",
       },
     },
+
     {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      title: "Digital Business Card",
+      techStack: ["Side Project", "Next.js", "ReactJS", "CSS"],
       description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
+        "Digital Business Card",
+      logo: AnuIcon,
+      link: {
+        label: "Digital ID",
+        href: "https://business-card-virid-omega.vercel.app/",
+      },
     },
   ],
 } as const;
--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -1,37 +1,14 @@
-import ConsultlyLogo from "./consultly.svg";
-import AmbitLogo from "./ambit.png";
-import BarepapersLogo from "./barepapers.svg";
-import BimLogo from "./bim.png";
-import CDGOLogo from "./cdgo.png";
-import ClevertechLogo from "./clevertech.png";
-import EvercastLogo from "./evercast.svg";
+import AnuIcon from "./anuICON.png"
+import CarHubLogo from "./carHubLogo.svg"
 import Howdy from "./howdy.png";
-import JarockiMeLogo from "./jarocki.svg";
-import JojoMobileLogo from "./jojomobile.png";
-import MonitoLogo from "./monito.svg";
-import MobileVikingsLogo from "./mv.png";
-import NSNLogo from "./nsn.svg";
-import ParabolLogo from "./parabol.svg";
-import TastyCloudLogo from "./tastycloud.png";
-import YearProgressLogo from "./yearprogress.svg";
-import Minimal from "./minimal.svg";
+import NikeLogo from "./nike.ico"
+import TheStarLogo from "./theStarLogo.png"
 
 export {
-  ConsultlyLogo,
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  EvercastLogo,
+  AnuIcon,
+  CarHubLogo,
   Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  MonitoLogo,
-  MobileVikingsLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-  Minimal,
+  NikeLogo,
+  TheStarLogo,
+
 };
